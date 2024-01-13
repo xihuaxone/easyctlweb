8ebc317ef0980367324fce74860b838602330fba
xihua <xihuaxone@outlook.com>
2024-01-14 00:18:59 +0800

    go back button;

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       {/*<header className="App-header">*/}
-      {/*  <img src={"imgs/rem.jpg"} className="App-logo" alt="logo" />*/}
+      {/*  <img src={"imgs/rem.png"} className="App-logo" alt="logo" />*/}
       {/*</header>*/}
       <RouterConfig></RouterConfig>
     </div>
